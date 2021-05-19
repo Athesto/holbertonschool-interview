c992e488dd70705ea2a42cd547e7825ac694f64c
Athesto <gamez.live@gmail.com>
2021-05-19 12:47:53 -0500

    betty is happy

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -3,25 +3,29 @@
 
 /**
  * copy_grid - Print 3x3 grid
- * @grid: 3x3 grid
+ * @grid1: 3x3 grid
+ * @grid2: 3x3 grid
  *
  */
 static void copy_grid(int grid1[3][3], int grid2[3][3])
 {
-	int i,j;
+	int i, j;
+
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			grid1[i][j] = grid2[i][j];
 }
 
 /**
- * copy_grid - Print 3x3 grid
+ * is_stable - Print 3x3 grid
  * @grid: 3x3 grid
+ * Return: stable=1 nostable=0
  *
  */
 static int is_stable(int grid[3][3])
 {
-	int i,j, stable = 1;
+	int i, j, stable = 1;
+
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			stable = stable && grid[i][j] <= 3;
@@ -52,16 +56,14 @@ static void print_grid(int grid[3][3])
 
 /**
  * sandpiles_sum - sum of elements
- * grid1 - input
- * grid2 - sum element
+ * @grid1: input
+ * @grid2: sum element
  *
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, stable = 0, *cell;
+	int i, j, stable = 1, *cell;
 
-	/* sum  */
-	stable = 1;
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -70,8 +72,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			*cell =  grid1[i][j] + grid2[i][j];
 			grid1[i][j] = *cell;
 			grid2[i][j] = *cell;
-			if (*cell > 4)
-				stable = 0;
+			stable = (*cell > 4);
 		}
 	}
 	stable = is_stable(grid1);
@@ -84,30 +85,17 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			for (j = 0; j < 3; j++)
 			{
 				cell =  &grid1[i][j];
-
 				if (grid2[i][j] >= 4)
 				{
 					*cell =  *cell - 4;
 					if (i < 2)
-					{
-						cell = &grid1[i + 1][j];
-						(*cell)++;
-					}
+						grid1[i + 1][j]++;
 					if (j < 2)
-					{
-						cell = &grid1[i][j + 1];
-						(*cell)++;
-					}
+						grid1[i][j + 1]++;
 					if (i > 0)
-					{
-						cell = &grid1[i - 1][j];
-						(*cell)++;
-					}
+						grid1[i - 1][j]++;
 					if (j > 0)
-					{
-						cell = &grid1[i][j - 1];
-						(*cell)++;
-					}
+						grid1[i][j - 1]++;
 				}
 			}
 		stable = is_stable(grid1);

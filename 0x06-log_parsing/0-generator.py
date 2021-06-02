6182959464ae8e13ce96a6239924faae2462f257
Athesto <gamez.live@gmail.com>
2021-06-02 16:13:12 -0500

    interview 0x06 0-stats.py

---
+++ 0x06-log_parsing/0-generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+import sys
+from time import sleep
+import datetime
+
+for i in range(10000):
+    #     sleep(random.random())
+    msg = "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
+    sys.stdout.write(msg.format(
+        random.randint(255, 400),
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
